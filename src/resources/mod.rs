4791444f8a92418c3b439ba5524e8ce5ebceb509
crategate <sevensidedsilver@gmail.com>
2024-06-21 13:14:01 -0400

    mod

---
+++ src/resources/mod.rs
@@ -0,0 +1,6 @@
+glib::wrapper! {
+    pub struct Window(ObjectSubclass<imp::Window>)
+        @extends gtk::ApplicationWindow, gtk::Window, gtk::Widget,
+        @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible, gtk::Buildable,
+                    gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
+}
