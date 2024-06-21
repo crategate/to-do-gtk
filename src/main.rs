f7d5d7ad8ca58e76487fce18d9811465e233d0a5
crategate <sevensidedsilver@gmail.com>
2024-06-21 13:28:00 -0400

    crates n stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+
+
+fn main() -> glib::ExitCode {
+    // Register and include resources
+    gio::resources_register_include!("todo_1.gresource")
+        .expect("Failed to register resources.");
+
+    // Create a new application
+    let app = Application::builder()
+        .application_id("org.gtk_rs.Todo1")
+        .build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    // Create a new custom window and present it
+    let window = Window::new(app);
+    window.present();
 }
