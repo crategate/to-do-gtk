33194a0d44be5f39a6490809cad1a89f1a954fea
crategate <sevensidedsilver@gmail.com>
2024-06-21 13:34:04 -0400

    imports and traits

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+mod task_object;
+mod task_row;
+mod window;
 
-
+use gtk::prelude::*;
+use gtk::{gio, glib, Application};
+use window::Window;
 fn main() -> glib::ExitCode {
     // Register and include resources
-    gio::resources_register_include!("todo_1.gresource")
-        .expect("Failed to register resources.");
+    gio::resources_register_include!("todo_1.gresource").expect("Failed to register resources.");
 
     // Create a new application
     let app = Application::builder()
--- src/window/imp.rs
+++ src/window/imp.rs
@@ -1,3 +1,9 @@
+use std::cell::RefCell;
+
+use glib::subclass::InitializingObject;
+use gtk::subclass::prelude::*;
+use gtk::{gio, glib, CompositeTemplate, Entry, ListView};
+
 // Object holding the state
 #[derive(CompositeTemplate, Default)]
 #[template(resource = "/org/gtk_rs/Todo1/window.ui")]
@@ -25,3 +31,26 @@ impl ObjectSubclass for Window {
         obj.init_template();
     }
 }
+
+// Trait shared by all GObjects
+impl ObjectImpl for Window {
+    fn constructed(&self) {
+        // Call "constructed" on parent
+        self.parent_constructed();
+
+        // Setup
+        let obj = self.obj();
+        obj.setup_tasks();
+        obj.setup_callbacks();
+        obj.setup_factory();
+    }
+}
+
+// Trait shared by all widgets
+impl WidgetImpl for Window {}
+
+// Trait shared by all windows
+impl WindowImpl for Window {}
+
+// Trait shared by all application windows
+impl ApplicationWindowImpl for Window {}
